a2c94b409c6ad3be4efaaf00a7083367592efbfb
Akvile-gc <akvile.gvildyte@gmail.com>
2023-03-10 21:02:22 +0200

    Adding book deletion option

---
+++ src/Controllers/BookControllers/BookDeletionController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+namespace PHP_Final_Project\Controllers\BookControllers;
+
+use PHP_Final_Project\Controllers\DataControllers\DataEntryController;
+use PHP_Final_Project\Controllers\DataControllers\DataExtractionController;
+use PHP_Final_Project\Frameworks\DIContainer;
+
+class BookDeletionController
+{
+    public function __construct(private DIContainer $container)
+    {
+    }
+
+    public function deleteBook()
+    {
+        $id = $_POST['id'];
+        $location = 'D:\CodeAcademy\Back End\PHP\PHP_Final_Project\src\DataBase\books.json';
+        $extraction = $this->container->get(DataExtractionController::class);
+        $books = $extraction->extractData($location);
+
+        unset($books[$id]);
+
+        $entry = $this->container->get(DataEntryController::class);
+
+        $entry->enterData($location, $books);
+        require __DIR__ . "./../../../views/istrynimas.php";
+
+
+    }
+}
