ce36d165c7f9192150722a6aa8bb43dfa31d07c4
Akvile-gc <akvile.gvildyte@gmail.com>
2023-03-11 21:40:54 +0200

    added deletion and edit controllers, based on BookChangeInterface

--- src/Controllers/BookControllers/BookDeletionController.php
+++ src/Controllers/BookControllers/BookDeletionController.php
@@ -5,27 +5,40 @@ namespace PHP_Final_Project\Controllers\BookControllers;
 use PHP_Final_Project\Controllers\DataControllers\DataEntryController;
 use PHP_Final_Project\Controllers\DataControllers\DataExtractionController;
 use PHP_Final_Project\Frameworks\DIContainer;
+use PHP_Final_Project\Models\BookChangeInterface;
+use PHP_Final_Project\Repositories\BookRepository;
 
-class BookDeletionController
+class BookDeletionController implements BookChangeInterface
 {
     public function __construct(private DIContainer $container)
     {
     }
 
-    public function deleteBook()
+    public function getBook(): void
     {
-        $id = $_POST['id'];
-        $location = 'D:\CodeAcademy\Back End\PHP\PHP_Final_Project\src\DataBase\books.json';
-        $extraction = $this->container->get(DataExtractionController::class);
-        $books = $extraction->extractData($location);
+        // TODO: Implement getBook() method.
+        $delete = $_POST['idDelete']; //this gets book's id
+        $repository = $this->container->get(BookRepository::class);
+        $books = $repository->getAll();
 
-        unset($books[$id]);
+        require __DIR__ . "./../../../views/istrynimas.php"; //directing to the page where we will update the book info
+    }
 
+    public function changeBook(): void
+    {
+        // TODO: Implement changeBook() method.
+        $delete = $_POST['idDelete']; //this gets book's id
+        $repository = $this->container->get(BookRepository::class);
+        $books = $repository->getAll();
+
+        //$books[$edit] is choosing the specific book.
+        // this logic bellow will allow to delete the book
+        unset($books[$delete]);
+
+        //putting the edited information back to the database
         $entry = $this->container->get(DataEntryController::class);
+        $entry->enterData('D:\CodeAcademy\Back End\PHP\PHP_Final_Project\src\DataBase\books.json', $books);
 
-        $entry->enterData($location, $books);
-        require __DIR__ . "./../../../views/istrynimas.php";
-
-
+        require __DIR__ . "./../../../views/duomenys_atnaujinti.php"; //getting the information page
     }
 }
--- src/Controllers/BookControllers/BookEditController.php
+++ src/Controllers/BookControllers/BookEditController.php
@@ -5,28 +5,44 @@ namespace PHP_Final_Project\Controllers\BookControllers;
 use PHP_Final_Project\Controllers\DataControllers\DataEntryController;
 use PHP_Final_Project\Controllers\DataControllers\DataExtractionController;
 use PHP_Final_Project\Frameworks\DIContainer;
+use PHP_Final_Project\Models\BookChangeInterface;
+use PHP_Final_Project\Repositories\BookRepository;
 
-class BookEditController
+class BookEditController implements BookChangeInterface
 {
     public function __construct(private DIContainer $container)
     {
     }
 
-    public function editBook()
+    public function getBook(): void
     {
-        $edit = $_POST['id'];
-        $location = 'D:\CodeAcademy\Back End\PHP\PHP_Final_Project\src\DataBase\books.json';
-        $extraction = $this->container->get(DataExtractionController::class);
-        $books = $extraction->extractData($location);
+        // TODO: Implement getBook() method.
+        $edit = $_POST['id']; //this gets book's id
+        $repository = $this->container->get(BookRepository::class);
+        $books = $repository->getAll(); //gets all books
 
-        $books[$edit]['id'] =
+        require __DIR__ . "./../../../views/redagavimas.php"; //directing to the page where we will update the book info
+    }
 
+    public function changeBook(): void
+    {
+        // TODO: Implement changeBook() method.
+        $edit = $_POST['id']; //this gets book's id
+        $repository = $this->container->get(BookRepository::class);
+        $books = $repository->getAll(); //gets all books
 
+        //$books[$edit] is choosing the specific book.
+        // this logic bellow will allow to change the book's current values to the edited ones
+        $books[$edit]['name'] = $_POST['edit_name'];
+        $books[$edit]['author'] = $_POST['edit_author'];
+        $books[$edit]['year'] = $_POST['edit_year'];
+        $books[$edit]['language'] = $_POST['edit_language'];
+        $books[$edit]['copies'] = $_POST['edit_copies'];
+
+        //putting the edited information back to the database
         $entry = $this->container->get(DataEntryController::class);
+        $entry->enterData('D:\CodeAcademy\Back End\PHP\PHP_Final_Project\src\DataBase\books.json', $books);
 
-        $entry->enterData($location, $books);
-
-        require __DIR__ . "./../../../views/redagavimas.php";
-
+        require __DIR__ . "./../../../views/duomenys_atnaujinti.php"; //getting the information page
     }
 }
