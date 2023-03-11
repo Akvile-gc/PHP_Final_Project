40eb83c6e48d5ab3c39ee0156f630b280faf2393
Akvile-gc <akvile.gvildyte@gmail.com>
2023-03-11 21:37:49 +0200

    new interface for changes - edit and delete

--- src/Models/BookChangeInterface.php
+++ src/Models/BookChangeInterface.php
@@ -3,23 +3,8 @@ declare(strict_types = 1);
 
 namespace PHP_Final_Project\Models;
 
-use PHP_Final_Project\Controllers\DataControllers\DataExtractionController;
-use PHP_Final_Project\Frameworks\DIContainer;
-
-class BookChangeModel
+interface BookChangeInterface
 {
-    public function __construct(private DIContainer $container)
-    {
-    }
-
-    public function getBook()
-    {
-        $edit = $_POST['id']; //this gets book's id
-        $location = 'D:\CodeAcademy\Back End\PHP\PHP_Final_Project\src\DataBase\books.json';
-        $extraction = $this->container->get(DataExtractionController::class);
-        $books = $extraction->extractData($location);
-
-        require __DIR__ . "./../../../views/redagavimas.php"; //directing to the page where we will update the book info
-
-    }
+    public function getBook():void;
+    public function changeBook():void;
 }
