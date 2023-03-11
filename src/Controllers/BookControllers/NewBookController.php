fb4d92f99dc829b2d1bb8a99362943bbfb16c989
Akvile-gc <akvile.gvildyte@gmail.com>
2023-03-11 21:57:37 +0200

    added repository

--- src/Controllers/BookControllers/NewBookController.php
+++ src/Controllers/BookControllers/NewBookController.php
@@ -6,22 +6,25 @@ namespace PHP_Final_Project\Controllers\BookControllers;
 use PHP_Final_Project\Controllers\DataControllers\DataToModelController;
 use PHP_Final_Project\Controllers\DataControllers\DataValidatorController;
 use PHP_Final_Project\Frameworks\DIContainer;
+use PHP_Final_Project\Models\BookActionModel;
 use PHP_Final_Project\Models\BookException;
 
-class NewBookController
+
+class NewBookController extends BookActionModel
 {
-    public function __construct(public DIContainer $container)
+    public function __construct(protected DIContainer $container)
     {
     }
 
-    public function set()
+    public function get()
     {
         $message = '';
         require __DIR__ . './../../../views/knyga.php';  //getting initial form to fill in
     }
 
-    public function add()
+    public function action()
     {
+        // TODO: Implement action() method.
         $validator = $this->container->get(DataValidatorController::class); // need to create one
 
         try {
@@ -33,46 +36,6 @@ class NewBookController
             $message = $e->getMessage(); //if unsuccessful
         }
 
-//        $location = __DIR__ . './../../DataBase/books.json';
-//
-//        //json decoding
-//        $decode = $this->container->get(DataExtractionController::class);
-//        $existingBooks = $decode->extractData($location);
-//
-//        //json encoding
-//        $encode = $this->container->get(DataEntryController::class);
-//
-//
-//        $name = $_POST['name'];
-//        $author = $_POST['author'];
-//        $year = (int)$_POST['year'];
-//        $language = $_POST['language'];
-//        $copies = (int)$_POST['copies'];
-//
-//
-//        foreach ($existingBooks as $book){
-//            if ($book['name'] === $name &&
-//                $book['author'] === $author &&
-//                $book['year'] === $year &&
-//                $book['language'] === $language){
-//                    $book['copies'] += $copies;
-//            } else {
-//                $id = count($existingBooks);
-//                $newBook = new BookModel($id, $name, $author, $year, $language, $copies);
-//                $enteredBook = [
-//                    'id'=>$newBook->getId(),
-//                    'name'=>$newBook->getName(),
-//                    'author'=>$newBook->getAuthor(),
-//                    'year'=>$newBook->getYear(),
-//                    'language'=>$newBook->getLanguage(),
-//                    'copies'=>$newBook->getCopies()
-//                ];
-//                $existingBooks[] = $enteredBook; //inserting new book to the books array
-//            }
-//        }
-//        $encode->enterData($location, $existingBooks); //encoding new data
-//
-//
         require __DIR__ . './../../../views/knyga.php';  //the form with the $message added
     }
 }
