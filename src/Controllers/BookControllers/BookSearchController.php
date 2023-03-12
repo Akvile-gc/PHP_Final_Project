41cc887120ded711767c8349c613f1a651c6417d
Akvile-gc <akvile.gvildyte@gmail.com>
2023-03-12 11:20:41 +0200

    prepared search logic

--- src/Controllers/BookControllers/BookSearchController.php
+++ src/Controllers/BookControllers/BookSearchController.php
@@ -4,6 +4,7 @@ namespace PHP_Final_Project\Controllers\BookControllers;
 
 use PHP_Final_Project\Frameworks\DIContainer;
 use PHP_Final_Project\Models\BookActionModel;
+use PHP_Final_Project\Repositories\BookRepository;
 
 class BookSearchController extends BookActionModel
 {
@@ -21,5 +22,38 @@ class BookSearchController extends BookActionModel
     public function action()
     {
         // TODO: Implement action() method.
+
+        $nameSearch = $_POST['name_search'];
+
+        $repository = $this->container->get(BookRepository::class);
+        $books = $repository->getAll();
+        $result = [];
+
+        if ($nameSearch){
+            foreach ($books as $book){
+                if ($book['name'] === $nameSearch){
+                    $result[] = $book;
+                }
+            }
+        }
+        require __DIR__ . './../../../views/knygu_rezultatai.php';  //getting search results
+    }
+
+    public function secondAction()
+    {
+        $authorSearch = $_POST['author_search'];
+
+        $repository = $this->container->get(BookRepository::class);
+        $books = $repository->getAll();
+        $result = [];
+
+        if ($authorSearch){
+            foreach ($books as $book){
+                if ($book['author'] === $authorSearch){
+                    $result[] = $book;
+                }
+            }
+        }
+        require __DIR__ . './../../../views/autoriu_rezultatai.php';  //getting search results
     }
 }
