c871dbaf880ac29e635032941b43c93dd2a35adf
Akvile-gc <akvile.gvildyte@gmail.com>
2023-03-12 16:16:56 +0200

    added initial page creation model

---
+++ src/Models/NewPageModel.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+namespace PHP_Final_Project\Models;
+
+use PHP_Final_Project\Frameworks\DIContainer;
+
+class NewPageModel
+{
+    public function __construct(protected DIContainer $container)
+    {
+    }
+
+    public function getNewPage(){
+
+    }
+}
